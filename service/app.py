9864d2a5c9cf5e9b2958ccf00c2d9bc6243e2265
nbp <splinesreticulating@users.noreply.github.com>
2025-09-13 13:36:07 -0700

    Add remaining keys to lookup table

--- service/app.py
+++ service/app.py
@@ -21,11 +21,19 @@ app = FastAPI(title="Essentia Audio Analyzer")
 
 
 CAMELOT = {
-  "C major":"8B","A minor":"8A","G major":"9B","E minor":"9A","D major":"10B","B minor":"10A",
-  "A major":"11B","F# minor":"11A","E major":"12B","C# minor":"12A","B major":"1B","G# minor":"1A",
-  "F# major":"2B","D# minor":"2A","C# major":"3B","A# minor":"3A","G# major":"4B","F minor":"4A",
-  "D# major":"5B","Eb major":"5B","C minor":"5A","A# major":"6B","Bb major":"6B","G minor":"6A",
-  "F major":"7B","D minor":"7A"
+    # Major keys (B suffix)
+    "C major": "8B", "G major": "9B", "D major": "10B", "A major": "11B", 
+    "E major": "12B", "B major": "1B", "F# major": "2B", "Gb major": "2B",
+    "C# major": "3B", "Db major": "3B", "G# major": "4B", "Ab major": "4B",
+    "D# major": "5B", "Eb major": "5B", "A# major": "6B", "Bb major": "6B",
+    "F major": "7B",
+    
+    # Minor keys (A suffix) 
+    "A minor": "8A", "E minor": "9A", "B minor": "10A", "F# minor": "11A",
+    "Gb minor": "11A", "C# minor": "12A", "Db minor": "12A", "G# minor": "1A",
+    "Ab minor": "1A", "D# minor": "2A", "Eb minor": "2A", "A# minor": "3A",
+    "Bb minor": "3A", "F minor": "4A", "C minor": "5A", "G minor": "6A",
+    "D minor": "7A"
 }
 
 class PathReq(BaseModel):
